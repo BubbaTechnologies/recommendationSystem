2332fdd11cc58299db077ea714651154c99a0a74
Matthew Groholski <mgrohols@asu.edu>
2023-05-24 10:55:40 -0700

    Progress

---
+++ models/tools.py
@@ -0,0 +1,30 @@
+__gendersDict = {
+    "female":0,
+    "male":1,
+    "boy":2,
+    "girl":3,
+    "kid":4,
+    "unisex":5
+}
+
+__typeDict = {
+   "top":0,
+   "bottom":1,
+   "shoes":2,
+   "underclothing":3,
+   "jacket":4,
+   "skirt":5,
+   "one piece":6,
+   "accessory":7,
+   "swimwear":8,
+   "sleepwear":9,
+   "other":10,
+   "dress":11,
+   "set":12
+}
+
+def genderToInt(string: str)->int:
+    return __gendersDict[string.lower()]
+
+def typeToInt(string: str)->int:
+    return __typeDict[string.lower()]
