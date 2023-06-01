7126eaa55d1e7f9e2b98c3fa82cd0e004a5b266c
Matthew Groholski <mgrohols@asu.edu>
2023-06-01 10:05:18 -0700

    Created writer_lock with asyncio

--- src/models/ReaderWriterLock.py
+++ src/models/ReaderWriterLock.py
@@ -1,24 +1,26 @@
 import threading
+import asyncio
 
 class ReaderWriterLock:
     def __init__(self):
         self.lock = threading.RLock()
+        self.writer_lock = asyncio.Lock()
         self.reader_count = 0
 
     async def acquire_read(self):
         with self.lock:
             self.reader_count += 1
             if self.reader_count == 1:
-                self.lock.acquire()
+                self.writer_lock.acquire()
 
     def release_read(self):
         with self.lock:
             self.reader_count -= 1
             if self.reader_count == 0:
-                self.lock.release()
+                self.writer_lock.release()
 
     async def acquire_write(self):
-        self.lock.acquire()
+        self.writer_lock.acquire()
 
     def release_write(self):
-        self.lock.release()
+        self.writer_lock.release()
